775cc1081d15351664331fecef23ee0e05892e5b
akshayudupa7 <akshayudupa7@gmail.com>
2024-03-29 23:11:47 +0530

    update

--- src/component/Section/index.tsx
+++ src/component/Section/index.tsx
@@ -3,82 +3,68 @@ import Image from "next/image";
 export default function Index() {
   return (
     <div className="grid grid-cols-12 w-[90%] mx-auto my-12">
-      <div className="col-span-2">
-        <div className=" rounded-full overflow-hidden h-[80px] w-[80px]  border-2 border-gray-300">
-          <Image
-            src="https://scontent.cdninstagram.com/v/t39.30808-6/434393255_18426342964063583_4568108945004089199_n.jpg?stp=dst-jpg_e35_p480x480&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMTI1eDIwMDAuc2RyIn0&_nc_ht=scontent.cdninstagram.com&_nc_cat=1&_nc_ohc=Ff5j-pqu_qgAX-B4mBP&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzMzMjM1MzI3NjIzMzgzNDkyMA%3D%3D.2-ccb7-5&oh=00_AfCNiPerL7Bzvlk8O3htU2apm93iZND3Pxybv5l3Ypkq_A&oe=66089DAB&_nc_sid=10d13b"
-            width={80}
-            height={80}
-            layout="responsive"
-            alt="this is image"
-          />
-        </div>
-        <h6 className="mt-2 font-bold">Hustle By Rcb</h6>
+      <div className=" col-span-2 rounded-full overflow-hidden h-[80px] w-[80px]  border-2 border-gray-300">
+        <Image
+          src="https://scontent.cdninstagram.com/v/t39.30808-6/434393255_18426342964063583_4568108945004089199_n.jpg?stp=dst-jpg_e35_p480x480&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMTI1eDIwMDAuc2RyIn0&_nc_ht=scontent.cdninstagram.com&_nc_cat=1&_nc_ohc=Ff5j-pqu_qgAX-B4mBP&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzMzMjM1MzI3NjIzMzgzNDkyMA%3D%3D.2-ccb7-5&oh=00_AfCNiPerL7Bzvlk8O3htU2apm93iZND3Pxybv5l3Ypkq_A&oe=66089DAB&_nc_sid=10d13b"
+          width={80}
+          height={80}
+          layout="responsive"
+          alt="this is image"
+          
+        />
+       
       </div>
-      <div className="col-span-2">
-      <div className="rounded-full overflow-hidden h-[80px] w-[80px] border-2 border-gray-300">
+      
+      <div className="col-span-2  rounded-full overflow-hidden h-[80px] w-[80px] border-2 border-gray-300">
         <Image
           src="https://scontent.cdninstagram.com/v/t39.30808-6/434220650_18426342508063583_6928834769465913160_n.jpg?stp=dst-jpg_e35_p480x480&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMTI1eDIwMDAuc2RyIn0&_nc_ht=scontent.cdninstagram.com&_nc_cat=111&_nc_ohc=TNGV83w2evEAX_1kz0I&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzMzMjM1MjQ1NzIzNzI1MTA4Ng%3D%3D.2-ccb7-5&oh=00_AfA_fEE5Mr50aEy4PRlZbfHpoY-zes28BPzUzvaW-MpwlA&oe=66083DA8&_nc_sid=10d13b"
           width={80}
           height={80}
           layout="responsive"
           alt="this is image"
+          
         />
       </div>
-        <h6 className="mt-2 font-bold">RCB x LEADERS</h6>
-      </div>
-      <div className="col-span-2">
-      <div className="rounded-full overflow-hidden h-[80px] w-[80px] border-2 border-gray-300">
+      <div className=" col-span-2  rounded-full overflow-hidden h-[80px] w-[80px] border-2 border-gray-300">
         <Image
           src="https://scontent.cdninstagram.com/v/t51.29350-15/394296540_679698887559821_792170722508084046_n.jpg?stp=dst-jpg_e35_p480x480&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMTc5eDIwOTYuc2RyIn0&_nc_ht=scontent.cdninstagram.com&_nc_cat=1&_nc_ohc=D6u6kWhREMwAX8KbB-I&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzIxOTgzMjAxMDg5ODE4MDM0Ng%3D%3D.2-ccb7-5&oh=00_AfAs4RQ3kGK61RIF3dtfv8pjOLqqm5mgy6GLYlxB0d9Kvg&oe=66085330&_nc_sid=10d13b"
           width={80}
           height={80}
           layout="responsive"
           alt="this is image"
+          
         />
       </div>
-        <h6 className="mt-2 font-bold">Leaders x RCB</h6>
-      </div>
-      <div className="col-span-2">
-      <div className=" rounded-full overflow-hidden h-[80px] w-[80px] border-2 border-gray-300">
+      <div className="col-span-2  rounded-full overflow-hidden h-[80px] w-[80px] border-2 border-gray-300">
         <Image
           src="https://scontent.cdninstagram.com/v/t51.29350-15/394381699_3493832550871530_3994861763484161995_n.jpg?stp=dst-jpg_e35_p480x480&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMTc5eDIwOTYuc2RyIn0&_nc_ht=scontent.cdninstagram.com&_nc_cat=106&_nc_ohc=I2XZJEV7UHkAX_fimKN&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzIxOTgzMzczOTk0MTQ5MzMwMg%3D%3D.2-ccb7-5&oh=00_AfCblzs4XRC0INcKMKnN80DK743WufI-J3P_lkanpzTUDw&oe=660834CD&_nc_sid=10d13b"
           width={80}
           height={80}
           layout="responsive"
           alt="this is image"
+          
         />
       </div>
-        <h6 className="mt-2 font-bold">Best of India</h6>
-      </div>
-      <div className="col-span-2">
-      <div className="rounded-full overflow-hidden h-[80px] w-[80px] border-2 border-gray-300">
+      <div className="col-span-2  rounded-full overflow-hidden h-[80px] w-[80px] border-2 border-gray-300">
         <Image
           src="https://scontent.cdninstagram.com/v/t51.29350-15/394179300_824194119441985_8342378750541322463_n.jpg?stp=dst-jpg_e35_p480x480&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMTc5eDIwOTYuc2RyIn0&_nc_ht=scontent.cdninstagram.com&_nc_cat=101&_nc_ohc=LY-BVT19b6gAX_ttEul&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzIxOTgzMzc0MDI4NTMyMzMxNA%3D%3D.2-ccb7-5&oh=00_AfDM9OUhLdUdA1d8dNFCaHGabT8wPAdlMNhVwxTVvs0Zeg&oe=66086566&_nc_sid=10d13b"
           width={80}
           height={80}
           layout="responsive"
           alt="this is image"
+          
         />
       </div>
-        <h6 className="mt-2 font-bold">Dash of RCB</h6>
-      </div>
-      <div className="col-span-2">
-      <div className=" rounded-full overflow-hidden h-[80px] w-[80px] border-2 border-gray-300">
+      <div className=" col-span-2  rounded-full overflow-hidden h-[80px] w-[80px] border-2 border-gray-300">
         <Image
           src="https://scontent.cdninstagram.com/v/t51.29350-15/394273655_342097471816787_5067441941933753109_n.jpg?stp=dst-jpg_e35_p480x480&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMTc5eDIwOTYuc2RyIn0&_nc_ht=scontent.cdninstagram.com&_nc_cat=111&_nc_ohc=3V07D5kwqiMAX804mG6&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzIxOTgzMzc0MDE1MTA0NDA2NA%3D%3D.2-ccb7-5&oh=00_AfDohwdIuVhXqwiEcE6u2I2MwaeOttxXvavAkWlYKctfLQ&oe=66083E7B&_nc_sid=10d13b"
           width={80}
           height={80}
           layout="responsive"
           alt="this is image"
+          
         />
       </div>
-        <h6 className="mt-2 font-bold">Bar & Cafe</h6>
-      </div>
-      
-        
-     
-      
     </div>
   );
 }
